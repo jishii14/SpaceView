c20fbad3939dabbf9c09188f0d8649e58d5b4789
jishii14 <jishii14@apu.edu>
2017-04-02 06:46:05 -0700

    Finished the application including text to voice and voice to text integration and a help button

--- SpaceView/code.js
+++ SpaceView/code.js
@@ -9,6 +9,19 @@ var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 var app = express();
 
+// var consumer = new soda.Consumer('explore.data.gov');
+//
+// consumer.query()
+//   .withDataset('644b-gaut')
+//   .limit(5)
+//   .where({ namelast: 'SMITH' })
+//   .order('namelast')
+//   .getRows()
+//     .on('success', function(rows) { console.log(rows); })
+//     .on('error', function(error) { console.error(error); });
+
+
+
 app.use('/', express.static(__dirname + '/'));
 
 console.log('Listening on 8888');
